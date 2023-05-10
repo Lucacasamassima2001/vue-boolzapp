ee5a11f7f482911bc9984bd241f54b2c67bfa093
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-05-10 17:20:35 +0200

    start ex

---
+++ script.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data(){ //or function() 
+  
+       return {
+            
+       }
+    },
+
+    methods: {
+         
+    },
+    
+   });
+   
+   
+   app.mount(`#app`);
